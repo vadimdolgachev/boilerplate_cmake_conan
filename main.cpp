9ed67165544cc138930b4432c7795360b27c3c75
vadim <vadimdolgachev@gmail.com>
2021-08-28 14:35:59 +0700

    add http redirect operation

--- main.cpp
+++ main.cpp
@@ -49,21 +49,27 @@ class Session : public std::enable_shared_from_this<Session> {
     constexpr static int HTTT_VERSION = 11;
     constexpr static auto TIMEOUT_SECS = std::chrono::seconds(30);
     tcp::resolver resolver;
-    beast::ssl_stream<beast::tcp_stream> stream;
+    boost::optional<beast::ssl_stream<beast::tcp_stream>> stream;
     beast::flat_buffer buffer;
     boost::optional<http::request<http::empty_body>> request;
     http::response<http::string_body> response;
+    net::any_io_executor executor;
+    ssl::context &context;
 
 public:
-    explicit Session(net::any_io_executor ex, ssl::context &ctx)
-        : resolver(ex),
-          stream(ex, ctx) {
+    explicit Session(const net::any_io_executor& executor_, ssl::context &context_)
+        : resolver(executor_),
+          executor(executor_),
+          context(context_) {
+
     }
 
     void get(const Uri &uri) {
         fmt::print("get {}{}\n", uri.host, uri.target);
 
-        if (!SSL_set_tlsext_host_name(stream.native_handle(), uri.host.c_str())) {
+        stream = beast::ssl_stream<beast::tcp_stream>(executor, context);
+
+        if (!SSL_set_tlsext_host_name(stream->native_handle(), uri.host.c_str())) {
             beast::error_code ec{static_cast<int>(::ERR_get_error()),
                                  net::error::get_ssl_category()};
             std::cerr << ec.message() << "\n";
@@ -85,55 +91,55 @@ private:
     void onResolve(beast::error_code ec,
                    const tcp::resolver::results_type &results) {
         if (ec) {
-            return onFail(ec, "resolve");
+            return onError(ec, "resolve");
         }
 
-        for (auto i : results) {
+        for (const auto &i : results) {
             fmt::print("onResolve={}:{} {}\n", i.host_name(), i.service_name(), i.endpoint().address().to_string());
         }
 
-        beast::get_lowest_layer(stream).expires_after(TIMEOUT_SECS);
-        beast::get_lowest_layer(stream).async_connect(results,
-                                                      beast::bind_front_handler(&Session::onConnect,
-                                                                                shared_from_this()));
+        beast::get_lowest_layer(*stream).expires_after(TIMEOUT_SECS);
+        beast::get_lowest_layer(*stream).async_connect(results,
+                                                       beast::bind_front_handler(&Session::onConnect,
+                                                                                 shared_from_this()));
     }
 
     void onConnect(beast::error_code ec,
                    const tcp::resolver::results_type::endpoint_type &endpoint) {
         if (ec) {
-            return onFail(ec, "connect");
+            return onError(ec, "connect");
         }
 
-        fmt::print("onConnect {} {}{} {}\n", request->method_string().data(),
-                   boost::replace_all_copy((*request)[http::field::host].to_string(), "\r\n", "").data(),
-                   request->target().data(),
+        fmt::print("onConnect {} {}{} {}\n", request->method_string().to_string(),
+                   boost::replace_all_copy((*request)[http::field::host].to_string(), "\r\n", "").c_str(),
+                   request->target().to_string(),
                    endpoint.address().to_string());
 
-        stream.async_handshake(ssl::stream_base::client,
-                               beast::bind_front_handler(&Session::onHandshake,shared_from_this()));
+        stream->async_handshake(ssl::stream_base::client,
+                                beast::bind_front_handler(&Session::onHandshake,shared_from_this()));
     }
 
     void onHandshake(beast::error_code ec) {
         if (ec) {
-            return onFail(ec, "handshake");
+            return onError(ec, "handshake");
         }
 
         fmt::print("onHandshake\n");
 
-        beast::get_lowest_layer(stream).expires_after(TIMEOUT_SECS);
-        http::async_write(stream,
+        beast::get_lowest_layer(*stream).expires_after(TIMEOUT_SECS);
+        http::async_write(*stream,
                           *request,
                           beast::bind_front_handler(&Session::onWrite, shared_from_this()));
     }
 
     void onWrite(beast::error_code ec, std::size_t bytes) {
         if (ec) {
-            return onFail(ec, "write");
+            return onError(ec, "write");
         }
 
-        fmt::print("onWrite bytes={}, {}\n", bytes);
+        fmt::print("onWrite bytes={}\n", bytes);
 
-        http::async_read(stream,
+        http::async_read(*stream,
                          buffer,
                          response,
                          beast::bind_front_handler(&Session::onRead,
@@ -142,27 +148,32 @@ private:
 
     void onRead(beast::error_code ec, std::size_t bytes) {
         if (ec) {
-            return onFail(ec, "read");
+            return onError(ec, "read");
         }
 
-        fmt::print("onRead bytes={}\n", bytes);
-
-        std::string_view body = response.body();
-        fmt::print("body={}\n", body.substr(0,
-                                            std::min(static_cast<std::string::size_type>(100),
-                                                     body.length())));
-        beast::get_lowest_layer(stream).expires_after(TIMEOUT_SECS);
+        fmt::print("onRead {} bytes={}\n", response.result_int(), bytes);
+        if (response.result() == http::status::found || response.result() == http::status::moved_permanently) {
+            const auto uri = Uri::parse(response.find(http::field::location)->value().to_string().c_str());
+            fmt::print("redirect to host={}, port={}, target={}\n",
+                       uri.host, uri.port, uri.target);
+            get(uri);
+        } else {
+            std::string_view body = response.body();
+            fmt::print("body={}\n", body.substr(0,
+                                                std::min(static_cast<std::string::size_type>(100),
+                                                         body.length())));
+            beast::get_lowest_layer(*stream).expires_after(TIMEOUT_SECS);
+        }
     }
 
-    static void onFail(beast::error_code ec, char const *what) {
-        fmt::print("onFail what={}, message={}\n", what, ec.message());
+    static void onError(beast::error_code ec, char const *what) {
+        fmt::print("onError={}, message={}\n", what, ec.message());
     }
 };
 
-
 int main(int argc, char *argv[]) {
     const Uri &uri = Uri::parse(argc > 1 ? argv[1] : "https://www.google.com");
-    fmt::print("host={} ,port={}, target={}\n",
+    fmt::print("host={}, port={}, target={}\n",
                uri.host, uri.port, uri.target);
     ssl::context sslContext{ssl::context::tlsv12_client};
     load_root_certificates(sslContext);
